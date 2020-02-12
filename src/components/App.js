00107bf680f88b5750231eec7c3a40112affd8eb
Jason Behnke <contact@jasonbehnke.io>
2020-02-11 22:38:19 -0800

    Init

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "../styles/barebones/barebones.css"
+import "../styles/barebones/normalize.css"
+import "../styles/App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello Nagios!</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
