e08b498a8b0130b79ef5c7401d74fe2ec5f99e51
Nicholas J. Tacey <nic@Nicholass-MacBook-Pro.local>
2020-03-29 00:46:27 -0700

    Removed class "App"

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import TopCard from './topCard.jsx'
-import BottomCard from './bottomCard.jsx'
-import '../styles/normalize.css'
-import '../styles/App.css'
+import React from "react"
+import TopCard from "./topCard.jsx"
+import BottomCard from "./bottomCard.jsx"
+import "../styles/normalize.css"
+import "../styles/App.css"
 
 function App() {
   return (
-    <div className="container App">
+    <div className="container">
       <TopCard />
       <BottomCard />
     </div>
