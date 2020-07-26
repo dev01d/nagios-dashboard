3084a40508f30e7f7998d704de23d3fe77f69e05
Jason Behnke <contact@jasonbehnke.io>
2020-07-25 20:35:57 -0700

    Revised to consume API data

--- src/components/topCard.jsx
+++ src/components/topCard.jsx
@@ -1,36 +1,101 @@
-import React from "react"
+import React from 'react'
+import Clock from 'react-live-clock'
+import {
+  serviceTopOk,
+  hostTopOk,
+  serviceTopWarn,
+  hostTopWarn,
+  serviceTopCritical,
+  hostTopCritical,
+} from './Data'
 
-var d = new Date()
+export default function topCard() {
+  const {
+    serviceTopOkData,
+    isLoadingServiceTopOkData,
+    isErrorServiceTopOkData,
+  } = serviceTopOk()
+  const {
+    hostTopOkData,
+    isLoadingHostTopOkData,
+    isErrorHostTopOkData,
+  } = hostTopOk()
 
-function topCard() {
+  const {
+    serviceTopWarnData,
+    isLoadingServiceTopWarnData,
+    isErrorServiceTopWarnData,
+  } = serviceTopWarn()
+  const {
+    hostTopWarnData,
+    isLoadingHostTopWarnData,
+    isErrorHostTopWarnData,
+  } = hostTopWarn()
+
+  const {
+    serviceTopCriticalData,
+    isLoadingServiceTopCriticalData,
+    isErrorServiceTopCriticalData,
+  } = serviceTopCritical()
+  const {
+    hostTopCriticalData,
+    isLoadingHostTopCriticalData,
+    isErrorHostTopCriticalData,
+  } = hostTopCritical()
+
   return (
     <div className="cards">
       <div className="card gray">
         <h1>Time</h1>
-        <h2>{d.getHours() + ":" + d.getMinutes()}</h2>
+        <h2>
+          <Clock />
+        </h2>
       </div>
       <div className="card" id="ok">
         <h1>OK</h1>
-        <h2>Data</h2>
+        {isErrorServiceTopOkData || isErrorHostTopOkData ? (
+          <h2>Error</h2>
+        ) : (
+          <h2>
+            {isLoadingServiceTopOkData || isLoadingHostTopOkData
+              ? 'Loading'
+              : serviceTopOkData.recordcount + hostTopOkData.recordcount}
+          </h2>
+        )}
       </div>
       <div className="card" id="warning">
         <h1>Warning</h1>
-        <h2>Data</h2>
+        {isErrorServiceTopWarnData || isErrorHostTopWarnData ? (
+          <h2>Error</h2>
+        ) : (
+          <h2>
+            {isLoadingServiceTopWarnData || isLoadingHostTopWarnData
+              ? 'Loading'
+              : serviceTopWarnData.recordcount + hostTopWarnData.recordcount}
+          </h2>
+        )}
       </div>
       <div className="card" id="critical">
         <h1>Critical</h1>
-        <h2>Data</h2>
+        {isErrorServiceTopCriticalData || isErrorHostTopCriticalData ? (
+          <h2>Error</h2>
+        ) : (
+          <h2>
+            {isLoadingServiceTopCriticalData || isLoadingHostTopCriticalData
+              ? 'Loading'
+              : serviceTopCriticalData.recordcount +
+                hostTopCriticalData.recordcount}
+          </h2>
+        )}
       </div>
       <div className="card" id="pending">
         <h1>Pending</h1>
-        <h2>Data</h2>
+        <h2>0</h2>
       </div>
       <div className="card gray">
         <h1>Unknown</h1>
-        <h2>Data</h2>
+        <h2>0</h2>
       </div>
     </div>
   )
 }
-
-export default topCard
