20680952691800bb5cccb1d8f9624afa62554606
Jason Behnke <contact@jasonbehnke.io>
2020-12-18 15:08:05 -0800

    Rework server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
-const path = require('path')
-const express = require('express')
-const app = express()
-const publicPath = path.join(__dirname, '..', 'build')
-const port = process.env.PORT || 1234
+const path = require("path");
+const express = require("express");
+const app = express(); // create express app
 
-app.use(express.static(publicPath))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'))
-})
-app.listen(port, () => {
-  console.log('Server up')
-})
+const {
+  REACT_APP_SERVICE,
+  REACT_APP_HOST,
+  REACT_APP_OK,
+  REACT_APP_CRITICAL,
+  REACT_APP_WARNING,
+  REACT_APP_URL,
+  REACT_APP_APIKEY
+} = process.env;
+
+// add middlewares
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+
+// start express server on port 5000
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
