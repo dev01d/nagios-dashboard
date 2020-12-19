1a2c6c38c8977898f84d3ad5d127ac8d15f4fe10
Jason Behnke <contact@jasonbehnke.io>
2020-12-18 16:54:23 -0800

    Add docker

---
+++ docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -euo pipefail
+
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
+
+file_env 'REACT_APP_URL'
+file_env 'REACT_APP_APIKEY'
+
+exec "$@"
